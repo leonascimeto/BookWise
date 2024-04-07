d0502fd4f832a116d9661464546ca9212bc4712c
leo <nascimentoleo899@gmail.com>
2024-04-07 11:03:52 -0300

    add register book usecase

---
+++ src/application/usecase/RegisterBookUseCase.ts
@@ -0,0 +1,17 @@
+export default class RegisterBookUseCase {
+  constructor(readonly bookRepository: any) {}
+
+  async execute(input: Input) {
+    const { title, author, isbn, genre, quantity } = input;
+    if (!title || !author || !isbn || !genre || !quantity) throw new Error('Invalid input');
+    await this.bookRepository.save({ title, author, isbn, genre, quantity });
+  }
+}
+
+type Input = {
+  title: string;
+  author: string;
+  isbn: string;
+  genre: string;
+  quantity: number;
+};
---
+++ src/application/usecase/__test__/ResgisterBookUseCase.spec.ts
@@ -0,0 +1,35 @@
+import RegisterBookUseCase from '../RegisterBookUseCase';
+
+describe('RegisterBookUseCase', () => {
+  const bookRepository = {
+    save: jest.fn(),
+  };
+
+  const book = {
+    title: 'title',
+    author: 'author',
+    isbn: 'isbn',
+    genre: 'genre',
+    quantity: 1,
+  };
+
+  test('should register a a book', async () => {
+    const sut = new RegisterBookUseCase(bookRepository);
+
+    expect(await sut.execute(book)).toBeUndefined();
+    expect(bookRepository.save).toHaveBeenCalledWith(book);
+    expect(bookRepository.save).toHaveBeenCalledTimes(1);
+  });
+
+  test.each([
+    { title: '', author: 'author', isbn: 'isbn', genre: 'genre', quantity: 1 },
+    { title: 'title', author: '', isbn: 'isbn', genre: 'genre', quantity: 1 },
+    { title: 'title', author: 'author', isbn: '', genre: 'genre', quantity: 1 },
+    { title: 'title', author: 'author', isbn: 'isbn', genre: '', quantity: 1 },
+    { title: 'title', author: 'author', isbn: 'isbn', genre: 'genre', quantity: 0 },
+  ])('should throw an error when input is invalid', async payload => {
+    const sut = new RegisterBookUseCase(bookRepository);
+
+    await expect(sut.execute(payload)).rejects.toThrow('Invalid input');
+  });
+});
