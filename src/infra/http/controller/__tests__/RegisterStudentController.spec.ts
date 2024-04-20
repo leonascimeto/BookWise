9b2e1d2846a154de4974e9993031ab203241f0ea
leo <nascimentoleo899@gmail.com>
2024-04-20 15:24:17 -0300

    feat: :sparkles: add zod validation in registe student controler

--- src/infra/http/controller/__tests__/RegisterStudentController.spec.ts
+++ src/infra/http/controller/__tests__/RegisterStudentController.spec.ts
@@ -26,4 +26,45 @@ describe('RegisterStudentController', () => {
     const response = await sut.handle(httpRequest);
     expect(response).toEqual({ status: 400, body: { message: 'any_error' } });
   });
+
+  describe('StudentSchema zod', () => {
+    test('should return 422 when name is less than 2', async () => {
+      const sut = new RegisterStudentController(registerStudentUseCase);
+      const response = await sut.handle({ ...httpRequest, body: { ...httpRequest.body, name: '' } });
+
+      expect(response.status).toBe(422);
+      expect(response.body.errors.fieldErrors).toEqual({ name: ['Name needs to have at least 2 characters'] });
+    });
+
+    test('should return 422 when name is greater than 100', async () => {
+      const sut = new RegisterStudentController(registerStudentUseCase);
+      const response = await sut.handle({ ...httpRequest, body: { ...httpRequest.body, name: 'a'.repeat(101) } });
+
+      expect(response.status).toBe(422);
+      expect(response.body.errors.fieldErrors).toEqual({ name: ['Name must have less than 100 characters'] });
+    });
+
+    test('should return 422 when matriculation is less than 2', async () => {
+      const sut = new RegisterStudentController(registerStudentUseCase);
+      const response = await sut.handle({ ...httpRequest, body: { ...httpRequest.body, matriculation: '' } });
+
+      expect(response.status).toBe(422);
+      expect(response.body.errors.fieldErrors).toEqual({
+        matriculation: ['Matriculation needs to have at least 2 characters'],
+      });
+    });
+
+    test('should return 422 when matriculation is greater than 20', async () => {
+      const sut = new RegisterStudentController(registerStudentUseCase);
+      const response = await sut.handle({
+        ...httpRequest,
+        body: { ...httpRequest.body, matriculation: 'a'.repeat(21) },
+      });
+
+      expect(response.status).toBe(422);
+      expect(response.body.errors.fieldErrors).toEqual({
+        matriculation: ['Matriculation must have less than 20 characters'],
+      });
+    });
+  });
 });
